85dd63b32a0b77c57cbae224f7862a5fb9c069a8
Yawning Angel <yawning@torproject.org>
2014-06-14 22:42:35 +0000

    Fix build on GCC 4.9.0.
    Just potentially uninitialized warnings in the 3rd party command line
    option parsing code, probably spurious and should be ignored but,
    initialize them for now.

--- src/ext/optionparser.h
+++ src/ext/optionparser.h
@@ -1554,9 +1554,9 @@ inline bool Parser::workhorse(bool gnu, const Descriptor usage[], int numargs, c
 
     do // loop over short options in group, for long options the body is executed only once
     {
-      int idx;
+      int idx = 0;
 
-      const char* optarg;
+      const char* optarg = 0;
 
       /******************** long option **********************/
       if (handle_short_options == false || try_single_minus_longopt)
